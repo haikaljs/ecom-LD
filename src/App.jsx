10ef4f56cf4c3c8acf64eebf381841ad178c20b9
haikaljs <haikaljavascript@gmail.com>
2023-12-29 15:43:19 +0800

    Design Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 
-
+import Home from "./pages/Home";
 
 function App() {
-
-
   return (
-   <>Ecom</>
-  )
+    <>
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
