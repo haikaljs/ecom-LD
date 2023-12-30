07125ef4ae301eaf2064b5733a58792d30035705
haikaljs <haikaljavascript@gmail.com>
2023-12-30 18:27:00 +0800

    Register and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import ProductList from "./pages/ProductList";
 import ProductSingle from "./pages/ProductSingle";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <ProductSingle />
+      <Login />
     </>
   );
 }
