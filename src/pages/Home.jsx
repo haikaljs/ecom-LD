49be226e2f31b2a27c51509cc5900bcff441cfc9
haikaljs <haikaljavascript@gmail.com>
2023-12-30 12:13:09 +0800

    Newletter design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
+import Newslettr from "../components/Newslettr";
+
 
 const Home = () => {
   return (
     <div>
       <Announcement />
       <Navbar />
-      <Slider/>
+      <Slider />
+      <Newslettr />
     </div>
   );
 };
